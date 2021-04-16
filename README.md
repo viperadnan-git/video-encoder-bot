d32c0d0e25fb06e50fc13026db71ee676efa8689
viperadnan-git <viperadnan@gmail.com>
2021-04-16 15:43:34 +0530

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,32 @@
-# video-encoder-bot
-A Telegram bot to encode video files via ffmpeg.
+# Video Encoder Bot
+A Telegram bot to convert videos into x265/x264 format via ffmpeg.
+
+### Configuration
+Add values in environment variables or add them in [config.env.template](./config.env.template) and rename file to `config.env`.
+- `API_ID` - Get it by creating an app on [https://my.telegram.org](https://my.telegram.org)
+- `API_HASH` - Get it by creating an app on [https://my.telegram.org](https://my.telegram.org)
+- `BOT_TOKEN` - Get it by creating a bot on [https://t.me/BotFather](https://t.me/BotFather)
+- `SUDO_USERS` - Chat identifier of the sudo user. For multiple users use space as seperator.
+- `DOWNLOAD_DIR` - (Optional) Temporary download directory to keep downloaded files.
+
+### Configuring Encoding Format
+To change the ffmpeg profile edit them in [ffmpeg_utils.py](./ffmpeg_utils.py)
+
+### Installing Requirements
+Install the required Python Modules in your machine.
+```sh
+apt-get -qq install ffmpeg
+pip3 install -r requirements.txt
+```
+### Deployment
+With python3.7 or later.
+```sh
+python3 main.py
+```
+
+### Credits
+*Thanks to [ShannonScott](https://gist.github.com/ShannonScott) for [transcode_h265.py](https://gist.github.com/ShannonScott/6d807fc59bfa0356eee64fad66f9d9a8)*
+
+### Copyright & License
+- Copyright &copy; 2021 &mdash; [Adnan Ahmad](https://github.com/viperadnan-git)
+- Licensed under the terms of the [GNU General Public License Version 3 &dash; 29 June 2007](./LICENSE)
---
+++ ffmpeg_utils.py
@@ -0,0 +1,71 @@
+import os
+import sys
+import json
+import time
+import ffmpeg
+from subprocess import call, check_output
+from hachoir.metadata import extractMetadata
+from hachoir.parser import createParser
+
+def get_codec(filepath, channel='v:0'):
+    output = check_output(['ffprobe', '-v', 'error', '-select_streams', channel,
+                            '-show_entries', 'stream=codec_name,codec_tag_string', '-of', 
+                            'default=nokey=1:noprint_wrappers=1', filepath])
+    return output.decode('utf-8').split()
+
+def encode(filepath):
+    basefilepath, extension = os.path.splitext(filepath)
+    output_filepath = basefilepath + '.HEVC' + '.mp4'
+    assert(output_filepath != filepath)
+    if os.path.isfile(output_filepath):
+        print('Skipping "{}": file already exists'.format(output_filepath))
+        return None
+    print(filepath)
+    # Get the video channel codec
+    video_codec = get_codec(filepath, channel='v:0')
+    if video_codec == []:
+        print('Skipping: no video codec reported')
+        return None
+    # Video transcode options
+    if video_codec[0] == 'hevc':
+        if video_codec[1] == 'hvc1':
+            print('Skipping: already h265 / hvc1')
+            return None
+        else:
+            # Copy stream to hvc1
+            video_opts = '-c:v copy -tag:v hvc1'
+    else:
+        # Transcode to h265 / hvc1
+        video_opts = '-c:v libx265 -crf 28 -tag:v hvc1 -preset fast -threads 8'
+    # Get the audio channel codec
+    audio_codec = get_codec(filepath, channel='a:0')
+    if audio_codec == []:
+        audio_opts = ''
+    elif audio_codec[0] == 'aac':
+        audio_opts = '-c:a copy'
+    else:
+        audio_opts = '-c:a aac -b:a 128k'
+    call(['ffmpeg', '-i', filepath] + video_opts.split() + audio_opts.split() + [output_filepath])
+    return output_filepath
+
+def get_thumbnail(in_filename, path, ttl):
+    out_filename = os.path.join(path, str(time.time()) + ".jpg")
+    open(out_filename, 'a').close()
+    try:
+        (
+            ffmpeg
+            .input(in_filename, ss=ttl)
+            .output(out_filename, vframes=1)
+            .overwrite_output()
+            .run(capture_stdout=True, capture_stderr=True)
+        )
+        return out_filename
+    except ffmpeg.Error as e:
+      return None
+
+def get_duration(filepath):
+  metadata = extractMetadata(createParser(filepath))
+  if metadata.has("duration"):
+    return metadata.get('duration').seconds
+  else:
+    return 0
