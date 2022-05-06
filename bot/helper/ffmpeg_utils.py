05a95b015ae34e061cd99212a0dccbdffb0f9d60
Mradul <nirusakimalaal@gmail.com>
2022-05-06 20:16:21 +0530

    Update ffmpeg_utils.py

--- bot/helper/ffmpeg_utils.py
+++ bot/helper/ffmpeg_utils.py
@@ -2,6 +2,7 @@ import os
 import sys
 import json
 import time
+inport ffmpeg
 from subprocess import call, check_output
 from hachoir.metadata import extractMetadata
 from hachoir.parser import createParser
