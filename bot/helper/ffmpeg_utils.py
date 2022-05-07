c0a415cbd0f4b3d76c7ae2426d95e03c5821f785
Mradul <nirusakimalaal@gmail.com>
2022-05-07 19:53:06 +0530

    Update ffmpeg_utils.py

--- bot/helper/ffmpeg_utils.py
+++ bot/helper/ffmpeg_utils.py
@@ -14,6 +14,7 @@ def encode(filepath):
     output_filepath = basefilepath + "R136A1_Encodes" + ".mkv"
     nam = output_filepath.replace("_", " ")
     nam = nam.replace(".", " ")
+    nam = nam.replace("/bot/downloads/", "")
     nam = nam + '.mkv'
     new_name = anitopy.parse(nam)
     anime_name = new_name["anime_title"]
@@ -25,7 +26,6 @@ def encode(filepath):
       episode_no = new_name["episode_number"]
       joined_string = f"{joined_string}" + f" [Episode {episode_no}]"
     og = joined_string + " [@R136a1Encodes]" + ".mkv"
-    og = og.replace("/bot/downloads/", "")
     fmd = '-map 0 -c:s copy'
     call(['ffmpeg', '-i', filepath] + fmd.split() + [output_filepath])
     os.remove(filepath)
