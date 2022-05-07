dfc7db41d49db877d37314d53c7a6107a5735d52
Mradul <nirusakimalaal@gmail.com>
2022-05-07 13:03:29 +0530

    Update __main__.py

--- bot/__main__.py
+++ bot/__main__.py
@@ -19,11 +19,11 @@ video_mimetype = [
   "video/mpeg"
   ]
 
-@app.on_message(filters.user(sudo_users) & filters.incoming & filters.command(['start', 'help']))
+@app.on_message(filters.user & filters.incoming & filters.command(['start', 'help']))
 async def help_message(app, message):
     message.reply_text(f"Hi {message.from_user.mention()}\n•I can encode Telegram files in x265/x264, just send me a video.\n•This Bot is Developed by @S136r136a1\n•**Simple, Easy and Convenient to use\nThanks")
 
-@app.on_message(filters.user(sudo_users) & filters.incoming & (filters.video | filters.document))
+@app.on_message(filters.user & filters.incoming & (filters.video | filters.document))
 async def encode_video(app, message):
     if message.document:
       if not message.document.mime_type in video_mimetype:
