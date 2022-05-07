0d08b329178c94f61aa8b81a5261bc93c81afbbf
Mradul <nirusakimalaal@gmail.com>
2022-05-07 11:36:25 +0530

    Update __main__.py

--- bot/__main__.py
+++ bot/__main__.py
@@ -2,6 +2,7 @@ from import asyncio
 from pyrogram import filters
 from bot import app, data, sudo_users
 from bot.helper.utils import add_task
+import asyncio
 
 video_mimetype = [
   "video/x-flv",
@@ -20,11 +21,11 @@ video_mimetype = [
   ]
 
 @app.on_message(filters.incoming & filters.command(['start', 'help']))
-def help_message(app, message):
+async def help_message(app, message):
     message.reply_text(f"Hi {message.from_user.mention()}\n**•I can encode Telegram files in x265/x264, just send me a video.**\n**•This Bot is Developed by @S136r136a1** \n•**Simple, Easy and Convenient to use**\n**Thanks**")
 
 @app.on_message(filters.user(sudo_users) & filters.incoming & (filters.video | filters.document))
-def encode_video(app, message):
+async def encode_video(app, message):
     if message.document:
       if not message.document.mime_type in video_mimetype:
         message.reply_text("```Invalid Video !\nMake sure its a valid video file.```", quote=True)
@@ -32,6 +33,6 @@ def encode_video(app, message):
     message.reply_text("```Added to queue...```", quote=True)
     data.append(message)
     if len(data) == 1:
-      add_task(message)
+     await add_task(message)
 
 app.run()
