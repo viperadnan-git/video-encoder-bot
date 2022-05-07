06cad76bbc20ddfb227f14b67d3dc4922e4f7ce8
unkown263 <75568317+unkown263@users.noreply.github.com>
2022-05-07 15:28:58 +0530

    Update __init__.py

--- bot/__init__.py
+++ bot/__init__.py
@@ -8,6 +8,7 @@ api_hash = os.environ.get("API_HASH")
 bot_token = os.environ.get("BOT_TOKEN")
 download_dir = os.environ.get("DOWNLOAD_DIR", "downloads/")
 sudo_users = list(set(int(x) for x in os.environ.get("SUDO_USERS").split()))
+ffmpeg = os.environ.get("FFMPEG", "")
 
 app = Client(":memory:", api_id=api_id, api_hash=api_hash, bot_token=bot_token)
 
